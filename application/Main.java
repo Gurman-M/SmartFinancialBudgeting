b0758346f32ee35d93450bf6f55637829c41b8e1
Gurman Mundi <gurmanmundi10@gmail.com>
2021-12-18 14:32:41 -0500

    Update Main.java
    resizing jframe

--- application/Main.java
+++ application/Main.java
@@ -2,7 +2,14 @@ import javax.swing.JFrame;
 
 public class Main { 
 
+    public static final int HEIGHT = 1000, WIDTH = 1000;
+
 	public static void main(String[] args) {
         JFrame jframe = new JFrame();
+        jframe.setTitle("Money Budgeter");
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setSize(WIDTH, HEIGHT);
+
+
     }
 }
