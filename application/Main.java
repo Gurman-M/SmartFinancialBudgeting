3f4da0cbea28c63eb75423420aed72f4c5c80fd9
Gurman Mundi <gurmanmundi10@gmail.com>
2021-12-18 16:15:14 -0500

    new code
    carried the team with this code

--- application/Main.java
+++ application/Main.java
@@ -1,4 +1,13 @@
+package moneyBudgeter;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Dictionary;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.WindowConstants;
@@ -9,6 +18,12 @@ public class Main {
 
 	public static void main(String[] args) {
         JFrame jframe = new JFrame();
+        JLabel label = new JLabel();
+        JButton budgetBtn = new JButton("Set Income");
+        JLabel incomeLabel = new JLabel("Yearly Income: Not set");
+        
+       // budgetBtn.setBounds(100, 200, 200, 100);
+        
         jframe.setTitle("Money Budgeter");
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setSize(WIDTH, HEIGHT);
@@ -22,19 +37,27 @@ public class Main {
 
         JSlider slider = new JSlider(JSlider.HORIZONTAL,
         SML_INC, LAG_INC, MID_INC);
-
-        int slid_val = slider.getValue();
-        slider.setValue(slid_val);
         
         slider.setMinorTickSpacing(5);
         slider.setMajorTickSpacing(20);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
+        
         JPanel panel = new JPanel();
         panel.add(slider);
+        panel.add(budgetBtn);
+        panel.add(incomeLabel);
         jframe.add(panel);
         jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jframe.setSize(500, 300);
+        
+        budgetBtn.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		    		incomeLabel.setText("Yearly Income " + slider.getValue() + " 000");
+		    }
+		});
+        
+        
         jframe.setVisible(true);
 
 
