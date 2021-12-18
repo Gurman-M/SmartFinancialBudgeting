1124f62a903a62b9432fe022b4b7e484ff5d861a
Gurman Mundi <gurmanmundi10@gmail.com>
2021-12-18 09:13:37 -0500

    Updated Main.java
    Testing java in vs code

--- application/Main.java
+++ application/Main.java
@@ -1,1 +1,12 @@
+public class Main { 
 
+	public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int a = 5;
+        String name = " test";
+
+        System.out.println(a + name);
+
+    }
+}
