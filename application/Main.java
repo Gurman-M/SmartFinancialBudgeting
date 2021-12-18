8d4db98c5273978dbdfba84f84e2fdeceef7ff12
Gurman Mundi <gurmanmundi10@gmail.com>
2021-12-18 14:31:04 -0500

    Update Main.java
    added jframe

--- application/Main.java
+++ application/Main.java
@@ -1,18 +1,8 @@
+import javax.swing.JFrame;
+
 public class Main { 
 
 	public static void main(String[] args) {
-        System.out.println("Hello");
-
-        int a = 5;
-        String name = " test";
-        int f=6;
-
-        String gurman = "gurman";
-
-        System.out.println("Hey");
-        System.out.println(a + name);
-        System.out.println("ILY");
-
-        System.out.println("lucas is a drone");
+        JFrame jframe = new JFrame();
     }
 }
