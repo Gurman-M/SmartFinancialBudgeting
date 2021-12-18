13a040e929ed3b42d424528bb10da978cceb3082
Gurman Mundi <gurmanmundi10@gmail.com>
2021-12-18 14:54:39 -0500

    Update Main.java

--- application/Main.java
+++ application/Main.java
@@ -1,16 +1,8 @@
-import javax.swing.JFrame;
+import java.util.*;
 
 public class Main { 
 
-    public static final int HEIGHT = 1000, WIDTH = 1000;
-
 	public static void main(String[] args) {
-        JFrame jframe = new JFrame();
-        jframe.setTitle("Money Budgeter");
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jframe.setSize(WIDTH, HEIGHT);
-        jframe.setVisible(true);
-
 
     }
 }
